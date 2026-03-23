5c05e5be5c8dc65dad4af9571b65246f382094d2
Grady Zhuo <gradyzhuo@gmail.com>
2026-03-23 17:13:00 +0800

    [FIX] migrate test file from XCTest to Swift Testing, fix CI build

--- Tests/NebulaTests/NebulaTests.swift
+++ Tests/NebulaTests/NebulaTests.swift
@@ -1,15 +1,5 @@
-import XCTest
+import Testing
 @testable import Nebula
 
-final class NebulaTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(Nebula().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+@Test func example() {
 }
